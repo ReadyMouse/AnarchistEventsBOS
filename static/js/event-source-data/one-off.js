97ec2c78e2ddb6ba68043f3c0ff1b7b432e3c527
ReadyMouse <elliot7096@gmail.com>
2023-10-12 15:10:22 -0400

    changing the banner to be QoWs. Also adding hopknott as a one-off that Vi did for me

--- static/js/event-source-data/one-off.js
+++ static/js/event-source-data/one-off.js
@@ -9,5 +9,56 @@
 import * as Utils from '../utils.js';
 import { default as FullCalendarEvent } from '../event.js';
 const OneOffEventSources = [
+	{
+		sourceType: 'one-off',
+		options: {},
+		sources: [
+			{
+				name: 'Hop Knot NH',
+				id: 'hop-knot-nh',
+				className: 'hop-knot-nh',
+				events: async function (fetchInfo, successCallback, failureCallback) {
+					var response = await fetch(Utils.useCorsProxy('https://hopknotnh.com/?post_type=hop-knot-events'));
+					var html = await response.text();
+					var doc = Utils.domparser.parseFromString(html, 'text/html');
+					var events = [];
+					doc.querySelectorAll('article.hop-knot-events').forEach(function ( el ) {
+						var [month, day, time, meridian] = el.querySelector('.hke-datetime')
+							.textContent.replaceAll("\n", ' ').trim().split(' ');
+						var t24 = Utils.convert12To24HourTime(`${time} ${meridian}`);
+						events.push({
+							title: el.querySelector('.entry-title').textContent,
+							start: new Date(`${month} ${day} ${new Date().getFullYear()} ${t24}`),
+							url: el.querySelector('[rel="bookmark"]').getAttribute('href'),
+							extendedProps: {
+								description: el.querySelector('.entry-summary p')?.textContent,
+								location: {
+									geoJSON: {
+										type: 'Point',
+										coordinates: ['42.9926758', '-71.4637792']
+									},
+									eventVenue: {
+										name: 'Hop Knot NH',
+										address: {
+											streetAddress: '1000 Elm Street',
+											addressLocality: 'Manchester',
+											addressRegion: 'NH',
+											postalCode: '03101',
+											addressCountry: 'United States'
+										},
+										geo: {
+											latitude: '42.9926758',
+											longitude: '-71.4637792'
+										}
+									}
+								}
+							}
+						});
+					});
+					successCallback(events);
+				}
+			}
+		]
+	}
 ];
 export default OneOffEventSources;
